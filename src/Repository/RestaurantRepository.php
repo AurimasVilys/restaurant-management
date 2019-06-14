41d46e1b5e8a9f07b466a676255d0ec528d36948
Aurimas Vilys <aurimas.vilys.96@gmail.com>
2019-06-14 19:29:13 +0300

    #2 Create RestaurantRepository and migration

---
+++ src/Repository/RestaurantRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Restaurant;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Restaurant|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Restaurant|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Restaurant[]    findAll()
+ * @method Restaurant[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class RestaurantRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Restaurant::class);
+    }
+}
