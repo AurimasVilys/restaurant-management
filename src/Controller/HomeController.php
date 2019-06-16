0d96ef5945cc57a36527714c1b8f4cda2a0d7fb7
Aurimas Vilys <aurimas.vilys.96@gmail.com>
2019-06-17 02:09:28 +0300

    Imporove dockerfile to install jpeg support. Add images for fixtures. Alter template files

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,8 +12,6 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'someVariable' => 'Restaurant management system',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
