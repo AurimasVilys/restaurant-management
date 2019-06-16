56a07865af31463c4cfc4e653a9a14a557a5efa1
Aurimas Vilys <aurimas.vilys.96@gmail.com>
2019-06-16 14:26:03 +0300

    #2 Add security bundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
