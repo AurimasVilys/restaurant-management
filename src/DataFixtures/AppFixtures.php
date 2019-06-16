719b420e23aa96ace5c53821638d8483b9a8aa84
Aurimas Vilys <aurimas.vilys.96@gmail.com>
2019-06-16 16:06:31 +0300

    Update views and forms.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,11 +3,26 @@
 namespace App\DataFixtures;
 
 use App\Entity\Restaurant;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    /**
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $restaurant = new Restaurant();
@@ -15,6 +30,16 @@ class AppFixtures extends Fixture
             ->setTitle('Užkandinė Avilys')
             ->setActive(true);
 
+
+        $user = new User();
+        $user->setName('Aurimas Vilys')
+            ->setEmail('aurimas@vilys.lt')
+            ->setRoles(['ROLE_USER'])
+            ->setPassword(
+                $this->passwordEncoder->encodePassword($user, 'aurimas')
+            );
+
+        $manager->persist($user);
         $manager->persist($restaurant);
         $manager->flush();
     }
